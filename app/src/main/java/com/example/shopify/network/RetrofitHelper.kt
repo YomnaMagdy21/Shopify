487994af54a4204e92d5f61ab3bc3241a722bb89
YomnaMagdy21 <93162207+YomnaMagdy21@users.noreply.github.com>
2024-06-07 02:15:53 +0300

    remove api keys

---
+++ app/src/main/java/com/example/shopify/network/RetrofitHelper.kt
@@ -0,0 +1,14 @@
+package com.example.shopify.network
+
+import com.example.shopify.utility.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitHelper {
+    //${Constants.apiKey}:${Constants.adminApiAccessToken}@
+//    const val BASE_URL="https://${Constants.storeURL}"
+//    var retrofitInstance= Retrofit.Builder()
+//        .baseUrl(BASE_URL)
+//        .addConverterFactory(GsonConverterFactory.create())
+//        .build()
+}
---
+++ app/src/main/java/com/example/shopify/utility/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.shopify.utility
+
+object Constants {
+//
+}
