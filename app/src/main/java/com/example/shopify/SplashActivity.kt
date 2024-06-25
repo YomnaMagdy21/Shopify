1d92b8dfee831e691d67617f47898218e196f976
YomnaMagdy21 <93162207+YomnaMagdy21@users.noreply.github.com>
2024-06-25 23:12:40 +0300

    add splash screen

---
+++ app/src/main/java/com/example/shopify/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.shopify
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(3000)
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
